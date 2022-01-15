735ac5cead8adb1d6d1a6b7493aeb5a8343eddfe
Param Shah <paramshah09@gmail.com>
2022-01-15 14:19:21 -0800

    feat: firebase class and ssnapshot

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import {useSession} from 'next-auth/react';
+import { doc, Firestore, onSnapshot } from "firebase/firestore";
+import { _Firebase } from '../utils/firebase';
+
+interface FirestoreUser {
+  name: string
+  devices: string[]
+  refresh_token?: string
+}
 
 export default function Home() {
   const {data: session} = useSession();
+  const [firebase] = useState(new _Firebase());
+  const [db, setDb] = useState<Firestore | null>(null);
+  const [data, setData] = useState<FirestoreUser | null>(null);
+
+  useEffect(() => {
+    setDb(firebase.db);
+  }, []);
+
+  useEffect(() => {
+    if (db)
+      onSnapshot(doc(db, "users", "bryanSucks"), (doc) => {
+        console.log("Current data: ", doc.data());
+        setData({
+          name: doc.data().name,
+          devices: doc.data().devices,
+        });
+      });
+  }, [db])
+
   return (
     <Layout>
       <h1>Hello {session?.user?.name} 👋</h1>
       <p>{session?.user?.email}</p>
+      <p>{data?.name}</p>
     </Layout>
   );
 }
--- packages/website/utils/firebase.ts
+++ packages/website/utils/firebase.ts
@@ -1,65 +1,21 @@
-import firebase from 'firebase';
-
-const FIREBASE_DEV = {
-  apiKey: 'AIzaSyC-2MLU3Q02juzp7z3WcprfAjzby9EXH4A',
-  authDomain: 'marfans-quiz-dev.firebaseapp.com',
-  projectId: 'marfans-quiz-dev',
-  storageBucket: 'marfans-quiz-dev.appspot.com',
-  messagingSenderId: '1061747901205',
-  appId: '1:1061747901205:web:22fee5912a8973fdd21f1c',
-  measurementId: 'G-2X770Z8408',
-};
+import firebase from 'firebase/compat/app';
+import { Firestore, getFirestore } from "firebase/firestore";
 
 const FIREBASE_PROD = {
-  apiKey: 'AIzaSyAhZHDNuy30rH-k59Cz1Km6kHQqtQxB4tQ',
-  authDomain: 'marfans-3b2fe.firebaseapp.com',
-  projectId: 'marfans-3b2fe',
-  storageBucket: 'marfans-3b2fe.appspot.com',
-  messagingSenderId: '22687257895',
-  appId: '1:22687257895:web:02b46ca0071862fda20502',
-  measurementId: 'G-SR0ZYYS7C8',
+  apiKey: "AIzaSyCgeY9tB_-okdEw18DWw44WwoPr4SHUfsc",
+  authDomain: "idea-hacks-2022.firebaseapp.com",
+  projectId: "idea-hacks-2022",
+  storageBucket: "idea-hacks-2022.appspot.com",
+  messagingSenderId: "14411425444",
+  appId: "1:14411425444:web:9e455baef0e1c20b34a831",
+  measurementId: "G-STQYVHZD7X"
 };
 
 const app =
   !firebase.apps.length
-    ? (firebase.initializeApp(process.env.env === 'production'
-      ? FIREBASE_PROD
-      : FIREBASE_DEV))
+    ? (firebase.initializeApp(FIREBASE_PROD))
     : firebase.app();
 
 export class _Firebase {
-  protected auth_user?: firebase.User;
-
-  public load(success: (...args: any[]) => void, fail: (...args: any[]) => void): void {
-    void firebase
-      .auth(app)
-      .setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-    firebase.auth(app).onAuthStateChanged((user: firebase.User | null) => {
-      if (user) {
-        this.auth_user = user;
-        const projectsDoc = firebase
-          .firestore(app)
-          .collection((process.env.env === 'production')
-            ? 'marfans-quiz'
-            : 'marfans-quiz-dev')
-          .doc('PROJECTS');
-        void projectsDoc.get().then((doc) => {
-          if (!doc.exists) void projectsDoc.set({});
-          success();
-        });
-      } else {
-        fail();
-      }
-    });
-  }
-
-  public auth(): firebase.auth.Auth {
-    return firebase.auth(app);
-  }
-
-  public signOut(): Promise<any> {
-    return firebase
-      .auth(app)
-      .signOut();
-  }
+  public readonly db: Firestore = getFirestore();
 }
