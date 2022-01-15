ad18bfa29b1d1c736a9c3b2f093d4acf543f8a28
BryanPan342 <bryanpan342@gmail.com>
2022-01-15 14:58:34 -0800

    feat: display albums from google photos

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -10,11 +10,25 @@ interface FirestoreUser {
   refresh_token?: string
 }
 
+import Grid from '@mui/material/Grid';
+
+import styles from '../styles/Home.module.scss';
+
+interface Album {
+  readonly id: string;
+  readonly title: string;
+  readonly productUrl: string;
+  readonly mediaItemsCount: number;
+  readonly coverPhotoBaseUrl: string;
+  readonly coverPhotoMediaItemId: string;
+}
+
 export default function Home() {
   const {data: session} = useSession();
   const [firebase] = useState(new _Firebase());
   const [db, setDb] = useState<Firestore | null>(null);
   const [data, setData] = useState<FirestoreUser | null>(null);
+  const [albums, setAlbums] = useState<Album[]>([]);
 
   useEffect(() => {
     setDb(firebase.db);
@@ -31,11 +45,47 @@ export default function Home() {
       });
   }, [db])
 
+  const fetchAlbums = async () => {
+    const res = await window.fetch('/api/list-albums');
+    if (!res.ok) { return; }
+    const data = await res.json();
+    setAlbums(data.albums);
+  }
+
+  useEffect(() => {
+    if (session) {
+      void fetchAlbums();
+    }
+  }, [session]);
+
+  useEffect(() => {
+    console.log(albums);
+  }, [albums]);
+
   return (
     <Layout>
-      <h1>Hello {session?.user?.name} 👋</h1>
-      <p>{session?.user?.email}</p>
-      <p>{data?.name}</p>
+      <div id={styles['home-container']}>
+        <h2>Hello {session?.user?.name} 👋</h2>
+        <p>{session?.user?.email}</p>
+
+
+        <div id={styles['albums-container']}>
+          <h3>Albums</h3>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {albums.map((album: Album, index) => {
+              return (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                  <div style={{padding: '16px', backgroundColor: 'white'}}>
+                    <img src={album.coverPhotoBaseUrl} style={{ width: '100%', height: 'auto'}}/>
+                    <h4>{album.title}</h4>
+                  </div>
+
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+      </div>
     </Layout>
   );
 }
