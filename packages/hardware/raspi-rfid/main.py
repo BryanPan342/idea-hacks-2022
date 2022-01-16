39e46c21f4e149f37593935e542c93b9030d06ea
Ashvin Nagarajan <ashvinagarajan@gmail.com>
2022-01-16 00:45:53 -0800

    firebase

--- packages/hardware/raspi-rfid/main.py
+++ packages/hardware/raspi-rfid/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env/ python
 
+import threading
 import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
@@ -38,6 +39,8 @@ def write(firestore_db):
     id, text = reader.read()
     print('writing tile id', id, 'for display on react')
 
+    firebase_dict = getFirebaseDict()
+
     payload = firebase_dict['payload']
     payload['tile_id'] = id
 
@@ -54,12 +57,12 @@ def button3_pressed(channel):
     firestore_db.collection(u'devices').document(u'mfrc522').update({'music': 3})
 
 if __name__ == '__main__':
-    setup()
+    button_pins = {11:1, 13:2, 15:3}
+    setup(button_pins)
     running = True
     firestore_db = firestore.client()
     reader = SimpleMFRC522()
-    button_pins = {11:1, 13:2, 15:3}
-    
+
     GPIO.add_event_detect(11, GPIO.FALLING, 
             callback=button1_pressed, bouncetime=100)
     GPIO.add_event_detect(13, GPIO.FALLING, 
@@ -67,8 +70,25 @@ if __name__ == '__main__':
     GPIO.add_event_detect(15, GPIO.FALLING, 
             callback=button3_pressed, bouncetime=100)
 
-    firebase_dict = getFirebaseDict()
-    readMode = firebase_dict['read_mode']
+    readMode = True
+
+    # Create an Event for notifying main thread.
+    callback_done = threading.Event()
+
+    # Create a callback on_snapshot function to capture changes
+    def on_snapshot(doc_snapshot, changes, read_time):
+        global readMode
+        for doc in doc_snapshot:
+            print(f'Received document snapshot: {doc.id}')
+            firebase_dict = firestore_db.collection(u'devices').document(u'mfrc522').get().to_dict()
+        readMode = firebase_dict['read_mode']
+        print(readMode)
+        callback_done.set()
+
+    doc_ref = firestore_db.collection(u'devices').document(u'mfrc522')
+
+    # Watch the document
+    doc_watch = doc_ref.on_snapshot(on_snapshot)
 
     while running==True:
         if readMode:
