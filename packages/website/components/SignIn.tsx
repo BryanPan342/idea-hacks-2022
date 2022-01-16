bdce157960e2ed116c78c0d461bafe4b6c25720b
BryanPan342 <bryanpan342@gmail.com>
2022-01-16 02:47:52 -0800

    fix: use router

--- packages/website/components/SignIn.tsx
+++ packages/website/components/SignIn.tsx
@@ -1,11 +1,13 @@
 import { signIn } from 'next-auth/react';
 import React from 'react';
+import {useRouter} from 'next/router';
 import styles from '../styles/SignIn.module.scss';
 
 export default function SignIn(): JSX.Element {
+  const router = useRouter();
   const login = () => {
     void signIn('google', {
-      callbackUrl: "https://idea-hacks-2022.vercel.app/"
+      callbackUrl: router.asPath,
     });
   };
   return (
